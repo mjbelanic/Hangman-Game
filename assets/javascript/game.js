8dd09b0b8160aae9514f658c4ce3d0726089fb6e
Matthew Belanic <Matthew Belanic>
2017-06-28 08:09:22 -0700

    Hangman Images, CSS styling, Javascript reset game function, minor HTML changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,7 @@
 // NEED TO IMPLEMENT:
-// Game Restart
-// Hints
-// Img getter and setter
-
-
-
-//Word arrays
-// var easyWords = ["word", "thanks" , "computer" , "class", "Mississippi", Placeholder];
-
+// css styling (review)
+// Handle capital letters
+// Get all pictures and descriptions
 
 
 //Word objects
@@ -18,12 +12,23 @@ function Word(nameValue, img, descr, hintValue){
 	this.hint = hintValue;
 };
 
-// var Placeholder = new Word("Placeholder" , "assets/images/EiffelTower.jpg", "This is a placeholder", "You added this to test");
-var Eiffel_Tower = new Word("Eiffel Tower", "assets/images/EiffelTower.jpg", "Paris, France", "Currently located in Paris");
-var Big_Ben = new Word("Big Ben", "assets/images/BigBen.jpg", "London, England" , "Famous Clock");
-var Berlin_Wall = new Word("Berlin Wall", null, "Berlin, Germany" , "Wall in Germany");
-var Leaning_Tower_of_Pisa = new Word("Leaning Tower of Pisa", "assets/images/LeaningTower.jpg", "Pisa, Italy", "A Crooked Tower")
-var hardWords = [Eiffel_Tower , Big_Ben, Berlin_Wall, Leaning_Tower_of_Pisa];
+// Word  objects
+var Eiffel_Tower = new Word("Eiffel Tower", "assets/images/EiffelTower.jpg", "", "Paris, France");
+var Big_Ben = new Word("Big Ben", "assets/images/BigBen.jpg", "", "London, England");
+var Leaning_Tower_of_Pisa = new Word("Leaning Tower of Pisa", "assets/images/LeaningTower.jpg", "","Tuscany, Italy");
+var Pyramids_of_Giza = new Word("Pyramids of Giza", "assets/images/GizaPyramids.jpg", "", "Ancient Egypt");
+var Lighthouse_Alexandria = new Word("Lighthouse of Alexandria", "assets/images/LighthouseAlexandria.png", "","Ancient Egypt");
+var HagiaSophia = new Word("Hagia Sophia", "assets/images/HagiaSophia.jpg", "" ,"Istanbul, Turkey");
+var GreatWall = new Word("Great Wall of China", "assets/images/GreatWall.jpg" ,"" ,"China");
+var PorcelainTower = new Word("Porcelain Tower of Nanjing" , "assets/images/PorcelainTower.jpg" , "" , "Nanjing, China");
+var Petra = new Word("Petra" , "assets/images/Petra.jpg" , "", "Ma'an Governorate, Jordan");
+var ChichenItza = new Word("Chichen Itza" , "assets/images/ChichenItza.jpg", "",  "Yucatan, Mexico");
+var MachuPicchu = new Word("Machu Picchu" , "assets/images/MachuPicchu.jpg", "", "Cuzco Region, Peru");
+var HimejiCastle = new Word("Himeji Castle", "assets/images/HimejiCastle.jpg", "", "Hyogo, Japan");
+
+//Word arrays
+var easyWords = [Eiffel_Tower , Big_Ben, Leaning_Tower_of_Pisa, Pyramids_of_Giza, Lighthouse_Alexandria];
+var hardWords = [HagiaSophia, GreatWall, PorcelainTower, Petra, ChichenItza, MachuPicchu, HimejiCastle];
 
 var HangmanObject = {
 	//Needed values
@@ -32,6 +37,8 @@ var HangmanObject = {
 	guessedLetters: [],
 	guessLimit: null,
 	resetButton: document.getElementById("resetButton"),
+	hintButton: document.getElementById("hintBtn"),
+	hintText: document.getElementById("hintText"),
 	
 	//Gets word from array and hides it
 	StartGame: function(wordArray){
@@ -40,13 +47,19 @@ var HangmanObject = {
 		this.guessedLetters = []; 
 		this.resetButton.style.display = "none";
 		this.hiddenWord = [];
+		this.hintButton.style.display = "inline";
+		this.hintText.innerHTML ="";
+		
 		document.getElementById("wordChecker").disabled = false;
 
+		//Get Random word and hide it.
 		document.getElementById("usedLetters").innerHTML = this.guessedLetters;
 		this.wordChoice = wordArray[Math.floor(Math.random() * wordArray.length)];
 		for (var i = 0 ; i < this.wordChoice.name.length ; i++) {
 			if(this.wordChoice.name[i] !== " "){
 				this.hiddenWord[i] = "_";
+			}else{
+				this.hiddenWord[i] = "&nbsp; &nbsp;";
 			}
 		}
 		document.getElementById("wordPlace").innerHTML = this.hiddenWord.join(" ");
@@ -62,7 +75,6 @@ var HangmanObject = {
 		imageSrc.style.display = "block";
 		document.getElementById(param5).innerHTML = this.wordChoice.description;
 		this.resetButton.style.display = "block";
-
 	},
 
 	//Checks if player has won the game
@@ -92,38 +104,41 @@ var HangmanObject = {
 	// Guess
 	checkLetter: function(){
 		var letter = document.getElementById("letterChoice").value;
-		document.getElementById("wrongLetter").innerHTML = "";
-		if(letter.length > 1){
-			document.getElementById("wrongLetter").innerHTML = "Please enter only 1 letter.";
-			document.getElementById("letterChoice").value = "";
-		}else if(letter.length <= 0){
-			document.getElementById("wrongLetter").innerHTML = "You did not enter anything.";
-		}else if(!isNaN(letter)){
-			document.getElementById("wrongLetter").innerHTML = "You entered a number. Enter a letter."
-			document.getElementById("letterChoice").value = "";
-		}else if(this.guessedLetters.includes(letter)){
-			document.getElementById("wrongLetter").innerHTML = "You have already picked this letter."
-			document.getElementById("letterChoice").value ="";
-		}else{	
-			this.guessedLetters.push(letter);
-			document.getElementById("usedLetters").innerHTML = this.guessedLetters;
-			if(this.wordChoice.name.includes(letter)){
-				for (var j = this.wordChoice.name.length - 1; j >= 0; j--) {
-					if(this.wordChoice.name[j] === letter){
-						this.hiddenWord[j] = letter;
-						document.getElementById("wordPlace").innerHTML = this.hiddenWord.join(" ");
-						document.getElementById("letterChoice").value = "";
+		var regex = /^[a-zA-Z]{1}$/;
+		if(regex.test(letter)){
+			if(this.guessedLetters.includes(letter)){
+				document.getElementById("wrongLetter").innerHTML = "You have already picked this letter."
+				document.getElementById("letterChoice").value ="";
+			//good char in word
+			}else{	
+				this.guessedLetters.push(letter);
+				document.getElementById("usedLetters").innerHTML = this.guessedLetters;
+				if(this.wordChoice.name.includes(letter)){
+					for (var j = this.wordChoice.name.length - 1; j >= 0; j--){
+						if(this.wordChoice.name[j] === letter){
+							this.hiddenWord[j] = letter;
+							document.getElementById("wordPlace").innerHTML = this.hiddenWord.join(" ");
+							document.getElementById("wrongLetter").innerHTML = "";
+							document.getElementById("letterChoice").value = "";
+						}
 					}
+				//good char not in work
+				}else{
+					this.guessLimit -= 1;
+					document.getElementById("wrongLetter").innerHTML = letter + " was not present in word.";
+					document.getElementById("letterChoice").value = "";
 				}
-			}else{
-				this.guessLimit -= 1;
-				document.getElementById("wrongLetter").innerHTML = letter + " was not present in word.";
-				document.getElementById("letterChoice").value = "";
 			}
+		// bad char
+		}else{
+			document.getElementById("wrongLetter").innerHTML = "You have not entered an appropriate character."
+			document.getElementById("letterChoice").value = "";
 		}
 		var gameOver = this.CheckForWin();
 		if(gameOver){
 			this.resetButton.style.display = "block";
+			this.hintButton.style.display = "none";
+			this.hintText.innerHTML = "";
 			document.getElementById("wordChecker").disabled = true;
 		}
 	},
@@ -135,15 +150,21 @@ var HangmanObject = {
 			var explanation = document.getElementById("outcomeWin")
 			explanation.style.display = "none";
 			var explanation2 = document.getElementById("outcomeLose");
-			explanation.style.display = "none"
+			explanation2.style.display = "none"
 			this.StartGame(hardWords);
 		}else{
 			var explanation = document.getElementById("outcomeWin")
 			explanation.style.display = "none";
 			var explanation2 = document.getElementById("outcomeLose");
-			explanation.style.display = "none"
+			explanation2.style.display = "none"
 			this.StartGame(easyWords);
 		}
+	},
+
+
+	DropHint: function(){
+		this.hintButton.style.display = "none";
+		this.hintText.innerHTML = "This structure is located in: " + this.wordChoice.hint;
 	}
 };
 
